2cc5ab059997f511b6e504fae6cb8754c6fe39d7
SuPeRMiNoR2 <superminor12@gmail.com>
2015-11-19 16:55:07 -0600

    Try formatting this correctly

--- ssdeploy.py
+++ ssdeploy.py
@@ -32,7 +32,7 @@ servermoddir = config["servermoddir"]
 print("\nStarting SuperSolderDeploy")
 print("Using solder instance: {0}".format(config["solderurl"]))
 print("Modpack name: {0}".format(config["modpackname"]))
-print("Server mods folder: ".format(config["servermoddir"]))
+print("Server mods folder: {0}".format(config["servermoddir"]))
 print("Currently installed modpack version: {0}".format(data["last"]))
 
 print("\nChecking solder for new version...")
